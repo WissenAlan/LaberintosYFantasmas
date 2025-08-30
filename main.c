cff90bfe20f64548d6fdc034626a32e782edfcfe
Alan Vicente <alancaifcm@gmail.com>
2025-08-30 20:53:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main()
-{
-    printf("#E########\n#..#P....#\n#..####..#\n#F..#....#\n#V#......#\n#..#####.#\n#...#....#\n#...#F#..#\n#F.......#\n########S#");
+int main() {
+    int cont = 0;
+    while (1) {
+        printf("#E########\n#..#P....#\n#..####..#\n#F..#....#\n#V#......#\n#..#####.#\n#...#....#\n#...#F#..#\n#F.......#\n########S#");
+        sleep(1);
+        cont++;
+        system("cls");
+        printf("\n%d\n", cont);
+    }
     return 0;
 }
