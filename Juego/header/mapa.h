a8e5bd13c445d4eb85a8265495695f8dc32b461b
MonasteryEr <erik.monastery3@gmail.com>
2025-10-17 22:12:48 -0300

    Coliciones reparadas

---
+++ Juego/header/mapa.h
@@ -0,0 +1,30 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include "jugador.h"
+#include "../estructuras/constantes.h"
+#define ESPACIADO 2
+#define VISITADO '.'
+#define CAMINO '.'
+#define CELDA ' '
+#define BONIFICACION 'B'
+typedef struct {
+    int fil;
+    int col;
+} Celdas;
+typedef struct{
+    char **mat;
+    int posyS;
+    int posxS;
+    int filMapa, colMapa;
+    int exit;
+} tMapa;
+void llenarMat(char**mat, int fil, int col);
+void mostrarMat(char**mat, int fil, int col);
+char**crearMatriz(int filas, int columnas);
+void eliminarMatriz(char**mat, int filas, int columnas);
+int crearLaberinto(tMapa *m, int fil, int col, tJugador*p, int, int, int);
+int buscarVecinos(char**mat, int fil, int col, Celdas*act, Celdas*vecinos);
+void checkend(tMapa *m, tJugador *p);
+int crearMapa(tMapa *m);
+int llenarMapa(tJugador *p, tMapa* m , int, int, int);
+#endif // FUNCIONES_H_INCLUDED
--- Juego/jugador.c
+++ Juego/jugador.c
@@ -1,51 +1,94 @@
-#include "headers/jugador.h"
-void moverjugador(tCola *colamov, char**mat, char movimiento, player *p) {
-    moves move;
-    move.posx = p->posx;
-    move.posy = p->posy;
-    if (movimiento == 'W' || movimiento == 'A' || movimiento == 'S' || movimiento == 'D') //SI TIENE MAYUS LAS CONVIERTE A MINUS
-        movimiento += 32;
-    if (movimiento == 'w' && mat[p->posx - 1][p->posy] != '#' && mat[p->posx - 1][p->posy] != 'E') {
-        p->posx--;
-        encolarMov(colamov, &move, ARRIBA);
-    }
-    if (movimiento == 's'  && mat[p->posx + 1][p->posy] != '#') {
-        p->posx++;
-        encolarMov(colamov, &move, ABAJO);
-    }
-    if (movimiento == 'a' && mat[p->posx][p->posy - 1] != '#') {
-        p->posy--;
-        encolarMov(colamov, &move, IZQUIERDA);
-    }
-    if (movimiento == 'd' && mat[p->posx][p->posy + 1] != '#') {
-        p->posy++;
-        encolarMov(colamov, &move, DERECHA);
-    }
+#include "header/jugador.h"
+void crearJugador(tJugador *p, int cantVidas) {
+    p->puntos = 0;
+    p->vidas = cantVidas;
+    p->posx = 0;
+    p->posy = 0;
 }
-void encolarMov(tCola *cola, moves *movimiento, int mov) {
-    movimiento->move = mov;
-    colaInsertar(cola, movimiento, sizeof(moves));
+unsigned getVidasJugador(tJugador *pJug){
+    return pJug->vidas;
 }
-void ai(tCola *colamov, char **mat, player *player, ghost *p) {
-    int try_w = state(mat, 1, player, p);
-    int try_s = state(mat, 2, player, p);
-    int try_a = state(mat, 3, player, p);
-    int try_d = state(mat, 4, player, p);
-    moves move;
-    move.posx = p->posx;
-    move.posy = p->posy;
-    if (try_w >= try_s && try_w >= try_a && try_w >= try_d)
-        encolarMov(colamov, &move, ARRIBA);
-    else {
-        if (try_s >= try_a && try_s >= try_d)
-            encolarMov(colamov, &move, ABAJO);
-        else {
-            if (try_a > try_d)
-                encolarMov(colamov, &move, IZQUIERDA);
-            else
-                encolarMov(colamov, &move, DERECHA);
+unsigned getPuntosJugador(tJugador *pJug){
+    return pJug->puntos;
+}
+void moverJugador(tCola *colamov, char **mat, tJugador *pJug) {
+    tMovimiento tmov;
+    tmov.posx = pJug->posx;
+    tmov.posy = pJug->posy;
+    tmov.entidad = JUGADOR;
+    int teclaInv = 1;
+    char tecla = getch(); // lee la tecla presionada
+    while (teclaInv == 1) {
+        teclaInv = 0;
+        switch (tecla) {
+        case 'W':
+        case 'w':
+            if (mat[pJug->posx - 1][pJug->posy] != PARED && mat[pJug->posx - 1][pJug->posy] != ENTRADA && mat[pJug->posx - 1][pJug->posy] != FANTASMA) {
+                pJug->posx--;
+                tmov.posx--;
+                encolarMov(colamov, &tmov, ARRIBA);
+            }
+            break;
+        case 's':
+        case 'S':
+            if (mat[pJug->posx + 1][pJug->posy] != PARED && mat[pJug->posx + 1][pJug->posy] != FANTASMA) {
+                pJug->posx++;
+                tmov.posx++;
+                encolarMov(colamov, &tmov, ABAJO);
+            }
+            break;
+        case 'a':
+        case 'A':
+            if (mat[pJug->posx][pJug->posy - 1] != PARED && mat[pJug->posx][pJug->posy-1] != FANTASMA) {
+                tmov.posy--;
+                pJug->posy--;
+                encolarMov(colamov, &tmov, IZQUIERDA);
+            }
+            break;
+        case 'd':
+        case 'D':
+            if (mat[pJug->posx][pJug->posy + 1] != PARED && mat[pJug->posx][pJug->posy + 1] != FANTASMA) {
+                tmov.posy++;
+                pJug->posy++;
+                encolarMov(colamov, &tmov, DERECHA);
+            }
+            break;
+        default:
+            teclaInv = 1;
+            break;
         }
     }
+}
+
+void encolarMov(tCola *cola, tMovimiento *pmov, int mov) {
+    pmov->movimiento = mov;
+    colaInsertar(cola, pmov, sizeof(tMovimiento));
+}
+
+void ai(tCola *colamov, char **mat, tJugador *pJug, tFantasma *pFant) {
+    int try_w = state(mat, 1, pJug, pFant);
+    int try_s = state(mat, 2, pJug, pFant);
+    int try_a = state(mat, 3, pJug, pFant);
+    int try_d = state(mat, 4, pJug, pFant);
+    tMovimiento movimiento;
+    int lado;
+    if (try_w >= try_s && try_w >= try_a && try_w >= try_d) {
+        pFant->posx--;
+        lado = ARRIBA;
+    } else if (try_s >= try_a && try_s >= try_d) {
+        pFant->posx++;
+        lado = ABAJO;
+    } else if (try_a > try_d) {
+        pFant->posy--;
+        lado = IZQUIERDA;
+    } else {
+        pFant->posy++;
+        lado = DERECHA;
+    }
+    movimiento.entidad = FANTASMA;
+    movimiento.posx = pFant->posx;
+    movimiento.posy = pFant->posy;
+    encolarMov(colamov, &movimiento, lado);
 }
 int abs(int numero) {
     if (numero > 0)
@@ -53,14 +96,11 @@ int abs(int numero) {
     else
         return -numero;
 }
-int state(char **mat, int trypos, player *p, ghost *f) {
+int state(char **mat, int trypos, const tJugador *pJug, const tFantasma *pFant) {
     int reward = 0;
-    int try_x = f->posx;
-    int try_y = f->posy;
-    int diff_x;
-    int diff_y;
-    int try_diff_x;
-    int try_diff_y;
+    int try_x = pFant->posx;
+    int try_y = pFant->posy;
+    int diff_x, diff_y, try_diff_x, try_diff_y;
     switch (trypos) {
     case 1:
         try_x--;
@@ -77,15 +117,15 @@ int state(char **mat, int trypos, player *p, ghost *f) {
     default:
         break;
     }
-    if (mat[try_x][try_y] == '#' || mat[try_x][try_y] == 'E' || mat[try_x][try_y] == 'S')
-        reward += -100;
+    if (mat[try_x][try_y] == PARED || mat[try_x][try_y] == ENTRADA || mat[try_x][try_y] == SALIDA)
+        reward += -1000;
     //if para que detecte algo lo modificamos para que detecte a el jugador;
     if (mat[try_x][try_y] == 'P')
-        reward += 100;
-    diff_x = abs(f->posx - p->posx);
-    diff_y = abs(f->posy - p->posy);
-    try_diff_x = abs(try_x - p->posx);
-    try_diff_y = abs(try_y - p->posy);
+        reward += 1000;
+    diff_x = abs(pFant->posx - pJug->posx);
+    diff_y = abs(pFant->posy - pJug->posy);
+    try_diff_x = abs(try_x - pJug->posx);
+    try_diff_y = abs(try_y - pJug->posy);
     if (try_diff_x < diff_x)
         reward += 5;
     if (try_diff_y < diff_y)
