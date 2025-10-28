2a93e33b52eda41628130212e56984110391ac06
GabrielGiannini <gabriel_giannini@yahoo.com.ar>
2025-10-28 12:21:09 -0300

    Update servidor.h

--- Servidor/servidor.h
+++ Servidor/servidor.h
@@ -6,13 +6,15 @@
 #define PUERTO 5302
 #define TAM_BUFFER 1024
 #define ARCH_JUG "jugadores.dat"
+#define ARCH_IDX "jugadores.idx"
+#define ARCH_PARTIDAS "partidas.dat"
+
 int init_winsock();
 
 SOCKET create_server_socket();
 
-void process_request(const char* request, char* response);
+int process_request(const char* request, char* response);
 
 void run_server();
-
 
 #endif // SERVIDOR_H_INCLUDED
