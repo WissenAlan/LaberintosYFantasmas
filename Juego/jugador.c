3a28487764dd6317b1cb90cd39113373b9ad57c4
Haziel Nervi <hnervi125@alumno.unlam.edu.ar>
2025-11-01 01:13:53 -0300

    balanceo ia

--- Juego/jugador.c
+++ Juego/jugador.c
@@ -106,12 +106,10 @@ int state(tMapa *mapa, int trypos,tJugador *pJug, const tFantasma *pFant)
     srand(time(NULL));
     trypos == ARRIBA ? try_x-- : (trypos == ABAJO ? try_x++ : (trypos == IZQUIERDA ? try_y-- : try_y++));
     if (mapa->mat[try_x][try_y] == PARED || mapa->mat[try_x][try_y] == ENTRADA || mapa->mat[try_x][try_y] == SALIDA || mapa->mat[try_x][try_y] == BONIFICACION)
-        reward+= -1000;
+        return -1000;
     //if para que detecte algo lo modificamos para que detecte a el jugador;
     if (mapa->mat[try_x][try_y] == JUGADOR)
         reward+= 100;
-
-    printf("%d",pJug->roundBuff);
     if(pJug->roundBuff > 0)
     {
         int auxcolumna=mapa->colMapa-1,auxfila=mapa->filMapa-1;
@@ -125,7 +123,7 @@ int state(tMapa *mapa, int trypos,tJugador *pJug, const tFantasma *pFant)
     }
     else
     {
-        if(rand() % 3 == 2)
+        if(rand() % 4 == 2)
         {
             diff_x = abs(pFant->posx - (pJug->posx+4));
             diff_y = abs(pFant->posy - (pJug->posy+4));
@@ -142,8 +140,6 @@ int state(tMapa *mapa, int trypos,tJugador *pJug, const tFantasma *pFant)
             try_diff_y = abs(try_y - pJug->posy);
         }
     }
-    if (mapa->mat[try_x][try_y] == JUGADOR)
-        reward+= 100;
     if (try_diff_x < diff_x)
         reward += 50;
     if (try_diff_y < diff_y)
