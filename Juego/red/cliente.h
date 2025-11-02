5ed851cef9462999b03927cb20bc4929cde41ede
GabrielGiannini <gabriel_giannini@yahoo.com.ar>
2025-11-02 18:56:12 -0300

    Update cliente.h

--- Juego/red/cliente.h
+++ Juego/red/cliente.h
@@ -28,6 +28,5 @@ void close_connection(SOCKET soc);
 void setSocketCliente(SOCKET s);
 void enviarPeticionCliente(const char *mensaje);
 
-
+void cerrarConexionCliente();
 #endif // CLIENTE_H_INCLUDED
-
